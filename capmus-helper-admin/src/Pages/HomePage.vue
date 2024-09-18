f37ec5101ce10fb75023050c25f50e73e33391cf
EatFan <eatfan0921@163.com>
2024-09-18 22:37:47 +0800

    设计后台管理系统主页面

--- capmus-helper-admin/src/Pages/HomePage.vue
+++ capmus-helper-admin/src/Pages/HomePage.vue
@@ -1,29 +1,79 @@
 <template>
-  <div class="home-page-container">
-    <h1>{{mount}}</h1>
-    <button @click="addMount">点击增加</button>
-    <button @click="reduceMount">点击减少</button>
+  <div :class="['app-wrapper', sidebarClass]">
+    <SideBar :isSidebarOpen="isSidebarOpen" />
+    <div class="main-container">
+      <div class="main-content">
+        <button @click="toggleSidebar">点我</button>
+
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
-import {ref} from "vue";
+import {computed, ref} from "vue";
 
-const mount = ref(0); // 创建一个mount变量，初始值0
+import SideBar from "@/components/SideBar.vue";
 
-const addMount = () =>{
-  mount.value ++;
-}
+// 定义一个状态来控制侧边栏的展开与折叠
+const isSidebarOpen = ref(true);
 
-/**
- *
- */
-const reduceMount = () => {
-  mount.value --;
-}
+// 根据侧边栏的状态来动态生成 class
+const sidebarClass = computed(() => {
+  return isSidebarOpen.value ? 'openSidebar' : 'hiddenSidebar';
+});
+
+// 切换侧边栏状态的方法
+const toggleSidebar = () => {
+  isSidebarOpen.value = !isSidebarOpen.value;
+};
+
 </script>
 
 
 <style scoped>
+.app-wrapper {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: row;
+}
+
+
+/**
+  展开sidebar后的main-container容器的样式
+ */
+.main-container {
+  width: 100%;
+  height: auto;
+  background: red;
+  overflow-y: auto;
+}
+
+
+/**
+  隐藏sidebar后的main-container容器的样式
+ */
+.hiddenSidebar .main-container {
+  width: 100%;
+  height: 2500px;
+  background: green;
+  overflow-y: auto;
+}
+
+
+.main-content {
+  margin-left: 200px;
+  width: 100%;
+  height: 2500px;
+}
+
+
+.hiddenSidebar .main-content {
+  width: 100%;
+  height: 2500px;
+  margin-left: 50px;
+  background: yellow;
+}
 
 </style>
---
+++ capmus-helper-admin/src/components/SideBar.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="sidebar-container" :style="{ width: isSidebarOpen ? '200px' : '50px' }" >
+
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+// 定义接收的样式类
+// eslint-disable-next-line no-unused-vars
+const props = defineProps({
+  isSidebarOpen: {
+    type: Boolean,
+    default: true
+  }
+});
+</script>
+
+<style scoped>
+.sidebar-container{
+  position: fixed;
+  width: 200px;
+  height: 100vh;
+  overflow: auto;
+  border: black solid 1px;
+  transition: width 0.3s ease-in-out;
+  display: block;
+  background: #304054;
+}
+
+.test {
+  width: 100%;
+  height: 300px;
+  background: yellow;
+  margin-bottom: 20px;
+}
+</style>
